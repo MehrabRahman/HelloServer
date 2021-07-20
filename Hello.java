9ad4b999b10afea9182319120c9a27a42504e257
MehrabRahman <mehrab.rahman@revature.com>
2021-07-20 11:43:23 -0500

    First commit

---
+++ Hello.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Hello {
+	public static void main(String[] args) {
+		// System.out.println("Hello, World!");
+		try (ServerSocket server = new ServerSocket(8080)) {
+			while (true) {
+				Socket request = server.accept();
+				PrintWriter writer = new PrintWriter(request.getOutputStream());
+				writer.println("HTTP/1.1 200 OK\r\nContent-Length: 14\r\nContent-Type: plain/text\r\n\r\nHello, World!");
+				writer.flush();
+				request.close();
+			}
+		} catch (IOException ex) {
+			System.err.println("Oops!");
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Hello World
+This is a starting project. The program itself is not important. The environment and context around it is.
+
+## Functional Requirements
+- User can call the program and receive a "Hello, World!"
+
+## Non-functional Requirements
+- The program should be written in Java 8+
+- The program should have instructions for building, development, & usage
+- The program should be well documented & tested
+- The program should have logs
+- The program should be managed by VCS (i.e. git)
+- The program should be hosted on a remote git platform (i.e. GitHub)
+
